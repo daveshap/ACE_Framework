a67d335a3b64d5054b0f8d6696215572037f6120
DataBass <josmith@junipercommerce.com>
2023-09-26 19:21:03 -0400

    Outline Main

--- demos/AceAF/main.py
+++ demos/AceAF/main.py
@@ -1,6 +1,14 @@
 import threading
 import time
+from agentforge.utils.storage_interface import StorageInterface
 
+class ACE:
+    def __init__(self):
+        self.storage = StorageInterface()
+        self.selectsouthbus = self.storage.select_collection("SouthBus")
+        self.loadsouthbus = self.storage.load_collection("SouthBus")
+        self.selectnorthbus = self.storage.select_collection("NorthBus")
+        self.loadnorthbus = self.storage.load_collection("NorthBus")
 def loop_1():
     while True:
         print("Loop 1")
@@ -25,3 +33,23 @@ t2.start()
 # However, do note that in this specific example, the main program will keep running indefinitely.
 while True:
     pass
+
+
+class LayersInit():
+
+    def __init__(self):
+        pass
+
+    def l1start(self):
+        l1 = L1AspirationalClass()
+        l1.start_sub_threads()
+
+    def l2start(self):
+        l2 = L2StrategyClass()
+        l2.start_sub_threads()
+
+    def threadlayers(self):
+        thread1 = threading.Thread(target=self.l1start)
+        thread2 = threading.Thread(target=self.l2start)
+        thread1.start()
+        thread2.start()
