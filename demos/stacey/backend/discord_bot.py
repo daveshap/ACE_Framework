aa5a759e3818ed1e12421a6e6ad193a66920c777
Henrik Kniberg <h@kniberg.com>
2023-09-27 10:10:38 +0200

    stacey discord bot uses gpt to generate responses

--- demos/stacey/backend/discord_bot.py
+++ demos/stacey/backend/discord_bot.py
@@ -1,6 +1,8 @@
 import os
 import discord
 from dotenv import load_dotenv
+import gpt
+import config
 
 load_dotenv()
 
@@ -9,9 +11,11 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in to discord as {client.user}')
+
 
 @client.event
 async def on_message(message):
@@ -19,10 +23,25 @@ async def on_message(message):
         return
 
     if 'stacey' in message.content.lower():
-        await message.channel.send('Hello! Stacey here.')
+        print(f"Got discord message from {message.author}: {message.content}")
+
+        conversation = [
+            {"role": "system", "content": config.system_message},
+            {"role": "user", "content": message.content}  # User's message
+        ]
+
+        try:
+            response = gpt.create_chat_completion('gpt-3.5-turbo', conversation)
+
+            print(f"  Responding to {message.author}: {response['content']}")
+            await message.channel.send(response['content'])
+        except Exception as e:
+            await message.channel.send(f"Damn! Something went wrong!: {str(e)}")
+
 
 def run():
     client.run(os.getenv("DISCORD_BOT_TOKEN"))
+
 
 if __name__ == '__main__':
     run()
