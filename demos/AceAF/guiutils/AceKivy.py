6c93efa6326c46d44e0c2d9e601eb493434e7a8d
DataBass <josmith@junipercommerce.com>
2023-09-28 23:05:33 -0400

    GUI Tweaks

--- demos/AceAF/guiutils/AceKivy.py
+++ demos/AceAF/guiutils/AceKivy.py
@@ -10,8 +10,10 @@ from flask import Flask, request, jsonify
 import threading
 import requests
 from kivy.lang import Builder
+from kivy.uix.scrollview import ScrollView
 
 app = Flask(__name__)
+
 
 
 @app.route('/api1', methods=['POST'])
@@ -43,6 +45,13 @@ def run_flask_app():
 
 
 class KivyApp(App):
+
+    def __init__(self, **kwargs):
+        super(KivyApp, self).__init__(**kwargs)
+        self.history_api1 = ""
+        self.history_api2 = ""
+        self.history_api3 = ""
+
     def build(self):
         self.main_layout = BoxLayout(orientation='vertical')
         self.tab_panel = TabbedPanel()
@@ -53,14 +62,34 @@ class KivyApp(App):
         self.tab3 = TabbedPanelItem(text='API 3')
 
         self.label_default = Label(text='This is the default tab.')
-        self.label_api1 = Label(text='Waiting for message on API 1...')
-        self.label_api2 = Label(text='Waiting for message on API 2...')
-        self.label_api3 = Label(text='Waiting for message on API 3...')
+
+        # Message histories
+        self.history_api1 = "Waiting for message on API 1...\n"
+        self.history_api2 = "Waiting for message on API 2...\n"
+        self.history_api3 = "Waiting for message on API 3...\n"
+
+        # API 1 ScrollView and Label
+        self.scrollview_api1 = ScrollView()
+        self.label_api1 = Label(text=self.history_api1, size_hint_y=None)
+        self.label_api1.bind(texture_size=self.label_api1.setter('size'))
+        self.scrollview_api1.add_widget(self.label_api1)
+
+        # API 2 ScrollView and Label
+        self.scrollview_api2 = ScrollView()
+        self.label_api2 = Label(text=self.history_api2, size_hint_y=None)
+        self.label_api2.bind(texture_size=self.label_api2.setter('size'))
+        self.scrollview_api2.add_widget(self.label_api2)
+
+        # API 3 ScrollView and Label
+        self.scrollview_api3 = ScrollView()
+        self.label_api3 = Label(text=self.history_api3, size_hint_y=None)
+        self.label_api3.bind(texture_size=self.label_api3.setter('size'))
+        self.scrollview_api3.add_widget(self.label_api3)
 
         self.tab_default.add_widget(self.label_default)
-        self.tab1.add_widget(self.label_api1)
-        self.tab2.add_widget(self.label_api2)
-        self.tab3.add_widget(self.label_api3)
+        self.tab1.add_widget(self.scrollview_api1)
+        self.tab2.add_widget(self.scrollview_api2)
+        self.tab3.add_widget(self.scrollview_api3)
 
         self.tab_panel.add_widget(self.tab_default)
         self.tab_panel.add_widget(self.tab1)
@@ -71,7 +100,7 @@ class KivyApp(App):
 
         # Chatbox and Send button
         self.chatbox = TextInput(hint_text='Enter a message...')
-        self.send_button = Button(text='Send', size_hint_x=None, width=125)
+        self.send_button = Button(text='Send', size_hint_x=None, width=100)
         self.send_button.bind(on_press=self.send_message)
 
         self.bottom_layout = BoxLayout(size_hint_y=None, height=44)
@@ -83,13 +112,16 @@ class KivyApp(App):
         return self.main_layout
 
     def update_label_api1(self, message):
-        self.label_api1.text = message
+        self.history_api1 += message + '\n'
+        self.label_api1.text = self.history_api1
 
     def update_label_api2(self, message):
-        self.label_api2.text = message
+        self.history_api2 += message + '\n'
+        self.label_api2.text = self.history_api2
 
     def update_label_api3(self, message):
-        self.label_api3.text = message
+        self.history_api3 += message + '\n'
+        self.label_api3.text = self.history_api3
 
     def send_message(self, instance):
         message = self.chatbox.text
