39b4ddf37b66237857b88c5d292f7333b93441dd
Henrik Kniberg <h@kniberg.com>
2023-09-30 09:05:00 +0200

    stacey: brain page (placeholder)

---
+++ demos/stacey/frontend/pages/brain/index.tsx
@@ -0,0 +1,11 @@
+// pages/watch/index.tsx
+import {Text} from '@chakra-ui/react';
+import React from "react";
+
+const IndexPage = () => {
+    return (
+        <Text>Placeholder watch page</Text>
+    );
+};
+
+export default IndexPage;
--- demos/stacey/frontend/pages/index.tsx
+++ demos/stacey/frontend/pages/index.tsx
@@ -1,85 +1,22 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import {Box, Textarea, VStack, Text, Container, Alert, Heading, Flex, Spinner, Image, Select} from '@chakra-ui/react';
-import ChakraUIRenderer from "chakra-ui-markdown-renderer";
-import ReactMarkdown from "react-markdown";
+// pages/index.tsx
+import {Box, Image, VStack} from '@chakra-ui/react';
+import NextLink from 'next/link';
+import React from "react";
 
-function Chat() {
-    const [messages, setMessages] = useState<Array<{ role: string; content: string }>>([]);
-    const [input, setInput] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [model, setModel] = useState('gpt-4');
-    const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
-
-    if (!backendUrl) {
-        return <Alert status="error">I don't know where the backend is! Please set env variable NEXT_PUBLIC_BACKEND_URL</Alert>;
-    }
-
-    const handleKeyPress = async (e: React.KeyboardEvent) => {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-            setMessages([...messages, { role: 'user', content: input }]);
-            setLoading(true);
-            setInput('');
-
-            try {
-                const response = await axios.post(backendUrl + "/chat", {
-                    model: model,
-                    conversation: [...messages, { role: 'user', content: input }],
-                });
-
-                setMessages([...messages, { role: 'user', content: input }, response.data]);
-            } catch (error) {
-                console.error('Error sending message:', error);
-            } finally {
-                setLoading(false);
-            }
-        }
-    };
-
+const IndexPage = () => {
     return (
-        <Container maxW="container.md" p={4}>
-            <VStack spacing={4} align="stretch" h="full">
-                <Heading mb={4}>Stacey</Heading>
-                <Box flex="1" overflowY="auto" p={3}>
-                    {messages.map((msg, index) => (
-                        <Flex key={index} mb={2} direction="column" align={msg.role === 'user' ? 'flex-end' : 'flex-start'}>
-                            <Flex align="center">
-                                {msg.role !== 'user' && <Image src="/images/stacey-160.png" borderRadius="full" boxSize="40px" mr={2} />}
-                                <Box p={2} rounded="md" bg={msg.role === 'user' ? 'blue.100' : 'gray.100'}>
-                                    <Text><b>{msg.role === 'user' ? 'You' : 'Stacey'}:</b></Text>
-                                    <ReactMarkdown  components={ChakraUIRenderer()} skipHtml>
-                                        {msg.content}
-                                    </ReactMarkdown>
-                                </Box>
-                            </Flex>
-                        </Flex>
-                    ))}
-                </Box>
-                {loading ? <Spinner /> :
-                    <>
-                        <Textarea
-                            value={input}
-                            onChange={(e) => setInput(e.target.value)}
-                            onKeyDown={handleKeyPress}
-                            h="60px"
-                            placeholder="Say something..."
-                        />
-                        <Select
-                                mb={4}
-                            placeholder="Select model"
-                            value={model}
-                            onChange={(e) => setModel(e.target.value)}
-                        >
-                            <option value="gpt-3.5-turbo">gpt-3.5-turbo</option>
-                            <option value="gpt-4">gpt-4</option>
-                        </Select>
-                    </>
-
-                }
+        <Box textAlign="center" fontSize="xl">
+            <VStack spacing={8} py={20}>
+                <Image src="/images/stacey-160.png" alt="Stacey" borderRadius="full"  mb={8} />
+                <NextLink href="/chat" passHref>
+                    Chat with Stacey
+                </NextLink>
+                <NextLink href="/brain" passHref>
+                    Look inside Stacey's brain
+                </NextLink>
             </VStack>
-        </Container>
+        </Box>
     );
-}
+};
 
-export default Chat;
+export default IndexPage;
