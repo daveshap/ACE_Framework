43ea28cb5de1bc5b0081b6c4f7d082cc4abf4a98
Henrik Kniberg <h@kniberg.com>
2023-10-01 09:01:22 +0200

    stacey: add missing requirement

--- demos/stacey/backend/util.py
+++ demos/stacey/backend/util.py
@@ -5,8 +5,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+def has_environment_variable(name):
+    value = os.getenv(name)
+    return value is not None and value.strip() != ""
+
+
 def get_environment_variable(name):
     value = os.getenv(name)
-    if value is None:
+    if value is None or value.strip() == "":
         raise EnvironmentError(f"{name} environment variable not set! Check your .env file.")
+
     return value
