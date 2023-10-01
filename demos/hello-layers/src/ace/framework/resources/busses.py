b7892114b8f03d37c832b5cea410004e66edbf31
Chad Phillips <chad@apartmentlines.com>
2023-10-01 19:50:13 -0400

    add busses and security resources

---
+++ demos/hello-layers/src/ace/framework/resources/busses.py
@@ -0,0 +1,83 @@
+import logging
+
+from ace.settings import Settings
+from ace.amqp.exchange import setup_exchange, teardown_exchange
+from ace.framework.resource import Resource
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+class BussesSettings(Settings):
+    pass
+
+
+class Busses(Resource):
+
+    @property
+    def settings(self):
+        return BussesSettings(
+            name="busses",
+            label="Busses",
+        )
+
+    def start_resource(self):
+        super().start_resource()
+        self.create_security_queues()
+        self.create_exchanges()
+
+    def stop_resource(self):
+        self.destroy_exchanges()
+        self.destroy_security_queues()
+        super().stop_resource()
+
+    def create_exchanges(self):
+        logger.debug(f"{self.labeled_name} creating exchanges...")
+        for idx, queue_name in enumerate(self.settings.layers):
+            self.create_exchange_by_index(idx, queue_name)
+        logger.debug(f"{self.labeled_name} queues created")
+
+    def create_exchange_by_index(self, idx, queue_name):
+        if idx > 0:
+            self.create_exchange(f"northbound.{queue_name}")
+        if idx < len(self.settings.layers) - 1:
+            self.create_exchange(f"southbound.{queue_name}")
+
+    def create_exchange(self, queue_name):
+        setup_exchange(
+            settings=self.settings,
+            connection=self.connection,
+            queue_name=queue_name,
+        )
+        logger.info(f" Created queue {queue_name} for resource {self.labeled_name}")
+
+    def destroy_exchanges(self):
+        logger.debug(f"{self.labeled_name} destroying exchanges...")
+        for idx, queue_name in enumerate(self.settings.layers):
+            self.destroy_exchange_by_index(idx, queue_name)
+        logger.debug(f"{self.labeled_name} exchanges destroyed")
+
+    def destroy_exchange_by_index(self, idx, queue_name):
+        if idx > 0:
+            self.destroy_exchange(f"northbound.{queue_name}")
+        if idx < len(self.settings.layers) - 1:
+            self.destroy_exchange(f"southbound.{queue_name}")
+
+    def destroy_exchange(self, queue_name):
+        teardown_exchange(
+            settings=self.settings,
+            connection=self.connection,
+            queue_name=queue_name,
+        )
+
+    def create_security_queues(self):
+        channel = self.connection.channel()
+        for layer in self.settings.layers:
+            queue_name = f"security.{layer}"
+            channel.declare_queue(queue_name, durable=True)
+
+    def destroy_security_queues(self):
+        channel = self.connection.channel()
+        for layer in self.settings.layers:
+            queue_name = f"security.{layer}"
+            channel.queue_delete(queue_name)
---
+++ demos/hello-layers/src/ace/framework/resources/security.py
@@ -0,0 +1,67 @@
+import logging
+import pika
+
+from ace.settings import Settings
+from ace.framework.resource import Resource
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+class SecuritySettings(Settings):
+    pass
+
+
+class Security(Resource):
+
+    @property
+    def settings(self):
+        return SecuritySettings(
+            name="security",
+            label="Security",
+        )
+
+    def start_resource(self):
+        super().start_resource()
+        self.subscribe_all_layers()
+
+    def stop_resource(self):
+        self.unsubscribe_all_layers()
+        super().stop_resource()
+
+    def send_message(self, layer, message, delivery_mode=2):
+        queue_name = f"security.{layer}"
+        self.publish_message(self, queue_name, message)
+
+    def publish_message(self, queue, message, delivery_mode=2):
+        self.channel.basic_publish(exchange='',
+                                   routing_key=queue,
+                                   body=message,
+                                   properties=pika.BasicProperties(
+                                       delivery_mode=delivery_mode,
+                                   ))
+
+    def post(self):
+        for layer in self.settings.layers:
+            self.post_layer(layer)
+
+    def post_layer(self, layer):
+        self.logger.debug(f"[{self.labeled_name}] sending POST to layer: {layer}")
+        message = self.build_message(message_type='ping')
+        self.send_message(self, layer, message)
+
+    def message_handler(self, channel: pika.channel.Channel, method: pika.spec.Basic.Deliver, properties: pika.spec.BasicProperties, body: bytes):
+        logger.debug(f"[{self.labeled_name}] received a message: {body.decode()}")
+
+    def subscribe_all_layers(self):
+        logger.debug(f"{self.labeled_name} subscribing to all layers...")
+        for queue_name in self.build_all_layer_queue_names():
+            # TODO: Consider separate message handlers?
+            self.channel.basic_consume(queue=queue_name, on_message_callback=self.message_handler)
+        logger.info(f"{self.labeled_name} Subscribed to all layers")
+
+    def unsubscribe_all_layers(self):
+        logger.debug(f"{self.labeled_name} unsubscribing from all layers...")
+        for queue_name in self.build_all_layer_queue_names():
+            self.channel.basic_cancel(queue_name)
+        logger.info(f"{self.labeled_name} Unsubscribed from all layers")
