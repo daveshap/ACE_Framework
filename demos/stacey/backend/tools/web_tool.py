63e6a82780c427738134900ee90805458f12e7d2
Henrik Kniberg <h@kniberg.com>
2023-10-03 11:18:01 +0200

    stacey: made a class WebTool and baseclass Tool

--- demos/stacey/backend/tools/web_tool.py
+++ demos/stacey/backend/tools/web_tool.py
@@ -1,8 +1,30 @@
-from bs4 import BeautifulSoup
+from typing import Dict, Any
+
 import requests
+from bs4 import BeautifulSoup
+
+from tools.tool import Tool
 
 
-def get_compressed_web_content(url):
+class WebTool(Tool):
+    def use_tool(self, params: Dict[str, Any]) -> str:
+        """
+        Fetches and compresses web content for a given URL.
+
+        Args:
+        - params (Dict[str, Any]): Must contain 'url' key with the URL to fetch.
+
+        Returns:
+        - str: Compressed web content.
+        """
+        url = params.get('url')
+        if not url:
+            raise ValueError("url not provided in parameters.")
+
+        return get_compressed_web_content(url)
+
+
+def get_compressed_web_content(url) -> str:
     response = requests.get(url)
     response.raise_for_status()  # Raise HTTPError for bad responses (4xx and 5xx)
     soup = BeautifulSoup(response.text, 'html.parser')
