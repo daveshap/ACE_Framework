f943c7388b6301bef9b7a752f0d748f253debe89
Chad Phillips <chad@apartmentlines.com>
2023-10-05 19:28:13 -0400

    refactor logger to be more robust, accept env var adjustments

--- demos/hello-layers/src/ace/logger.py
+++ demos/hello-layers/src/ace/logger.py
@@ -6,6 +6,14 @@ from ace import constants
 logging.basicConfig(level=logging.DEBUG)
 
 
+class ConsoleHandler(logging.StreamHandler):
+    pass
+
+
+class FileLogHandler(logging.FileHandler):
+    pass
+
+
 def get_log_level(level_str):
     level = logging.getLevelName(level_str)
     if not isinstance(level, int):
@@ -15,7 +23,8 @@ def get_log_level(level_str):
 
 # Set the log level for third party loggers separately.
 for base_logger in constants.THIRD_PARTY_LOGGERS:
-    log_level = os.getenv('ACE_THIRD_PARTY_LOG_LEVEL', constants.THIRD_PARTY_LOG_LEVEL)
+    # Docker Compose sets this to an empty string, so a 'falsy' check is needed here.
+    log_level = os.getenv('ACE_THIRD_PARTY_LOG_LEVEL') or constants.THIRD_PARTY_LOG_LEVEL
     logging.getLogger(base_logger).setLevel(get_log_level(log_level))
 
 
@@ -23,17 +32,15 @@ class Logger:
 
     def __new__(cls, name):
         logger = logging.getLogger(name)
-        # Prevent duplicate loggers.
-        if logger.hasHandlers():
-            return logger
-        logger.setLevel(logging.DEBUG)
-        log_console_handler = logging.StreamHandler()
-        log_level = os.getenv('ACE_LOG_LEVEL', constants.LOG_LEVEL)
-        log_console_handler.setFormatter(logging.Formatter(constants.LOG_FORMAT))
-        log_console_handler.setLevel(get_log_level(log_level))
-        logger.addHandler(log_console_handler)
-        if constants.LOG_FILEPATH:
-            log_file_handler = logging.FileHandler(constants.LOG_FILEPATH, "a")
+        log_level = os.getenv('ACE_LOG_LEVEL') or constants.LOG_LEVEL
+        logger.setLevel(get_log_level(log_level))
+        if not any(isinstance(handler, ConsoleHandler) for handler in logger.handlers):
+            log_console_handler = ConsoleHandler()
+            log_console_handler.setFormatter(logging.Formatter(constants.LOG_FORMAT))
+            log_console_handler.setLevel(get_log_level(log_level))
+            logger.addHandler(log_console_handler)
+        if constants.LOG_FILEPATH and not any(isinstance(handler, FileLogHandler) for handler in logger.handlers):
+            log_file_handler = FileLogHandler(constants.LOG_FILEPATH, "a")
             log_file_handler.setFormatter(logging.Formatter(constants.LOG_FORMAT))
             log_file_handler.setLevel(get_log_level(log_level))
             logger.addHandler(log_file_handler)
