4820763a6d26923dd5e7842c40b7ba34779a4fe8
Chad Phillips <chad@apartmentlines.com>
2023-10-05 16:56:24 -0400

    allow env overriders for logger

--- demos/hello-layers/src/ace/logger.py
+++ demos/hello-layers/src/ace/logger.py
@@ -1,10 +1,12 @@
 import logging
+import os
 
 from ace import constants
 
-# Set the logger for pika, aiormq separately.
+# Set the log level for third party loggers separately.
 for base_logger in constants.THIRD_PARTY_LOGGERS:
-    logging.getLogger(base_logger).setLevel(constants.THIRD_PARTY_LOG_LEVEL)
+    log_level = os.getenv('ACE_THIRD_PARTY_LOG_LEVEL', constants.THIRD_PARTY_LOG_LEVEL)
+    logging.getLogger(base_logger).setLevel(log_level)
 
 
 class Logger:
@@ -15,12 +17,13 @@ class Logger:
             return logger
         logger.setLevel(logging.DEBUG)
         log_console_handler = logging.StreamHandler()
+        log_level = os.getenv('ACE_LOG_LEVEL', constants.LOG_LEVEL)
         log_console_handler.setFormatter(logging.Formatter(constants.LOG_FORMAT))
-        log_console_handler.setLevel(constants.LOG_LEVEL)
+        log_console_handler.setLevel(log_level)
         logger.addHandler(log_console_handler)
         if constants.LOG_FILEPATH:
             log_file_handler = logging.FileHandler(constants.LOG_FILEPATH, "a")
             log_file_handler.setFormatter(logging.Formatter(constants.LOG_FORMAT))
-            log_file_handler.setLevel(constants.LOG_LEVEL)
+            log_file_handler.setLevel(log_level)
             logger.addHandler(log_file_handler)
         return logger
