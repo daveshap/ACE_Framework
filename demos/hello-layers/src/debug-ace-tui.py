995c7a3be0049a91830bc892e23f0f4fdf2c1480
Chad Phillips <chad@apartmentlines.com>
2023-10-15 20:32:06 -0400

    full crud for debug messages

--- demos/hello-layers/src/debug-ace-tui.py
+++ demos/hello-layers/src/debug-ace-tui.py
@@ -1,4 +1,3 @@
-import copy
 import yaml
 import functools
 from prompt_toolkit import Application
@@ -57,9 +56,11 @@ class DebugAceTui:
         self.active_layer_number = self.layers[0]
         self.dialog = None
         self.dialog_not_focused = Condition(lambda: self.dialog is None)
+        self.current_dialog_key = None
         self.text_area = TextArea(multiline=True)
         self.radio_list = RadioList(values=[('northbound', 'Northbound'), ('southbound', 'Southbound')])
         self.data_display = TextArea(lexer=PygmentsLexer(YamlLexer), read_only=True)
+        # self.current_messages_display = TextArea(lexer=PygmentsLexer(YamlLexer), read_only=True)
         self.kb = KeyBindings()
         self.app = Application(key_bindings=self.kb, full_screen=True, style=self.style)
 
@@ -85,48 +86,88 @@ class DebugAceTui:
 
         @self.kb.add('c', filter=self.dialog_not_focused)
         def _(event):
-            self.open_dialog("control", "Add CONTROL messages")
+            self.open_dialog("control", "Add/edit CONTROL messages")
 
         @self.kb.add('d', filter=self.dialog_not_focused)
         def _(event):
-            self.open_dialog("data", "Add DATA messages")
+            self.open_dialog("data", "Add DATA/edit messages")
 
         @self.kb.add('r', filter=self.dialog_not_focused)
         def _(event):
-            self.open_dialog("request", "Add REQUEST messages", include_direction=True)
+            self.open_dialog("request", "Add/edit REQUEST messages", include_direction=True)
 
         @self.kb.add('s', filter=self.dialog_not_focused)
         def _(event):
-            self.open_dialog("response", "Add RESPONSE messages", include_direction=True)
+            self.open_dialog("response", "Add/edit RESPONSE messages", include_direction=True)
 
         @self.kb.add('t', filter=self.dialog_not_focused)
         def _(event):
-            self.open_dialog("telemetry", "Add TELEMETRY messages")
+            self.open_dialog("telemetry", "Add/edit TELEMETRY messages")
 
         @self.kb.add('e', filter=self.dialog_not_focused)
         def _(event):
             self.clear_layer()
+
+        @self.kb.add('c-s', filter=~self.dialog_not_focused)
+        def _(event):
+            self.open_message_selector("edit")
+
+        @self.kb.add('c-d', filter=~self.dialog_not_focused)
+        def _(event):
+            self.open_message_selector("delete")
 
     def layer_kb_callback(self, layer):
         def callback(event):
             self.set_active_layer(layer)
         return callback
 
-    def open_dialog(self, key, title, include_direction=False):
+    def build_fields(self, include_direction=False):
         fields = []
         if include_direction:
             fields.append(Label(text='Direction:'))
             fields.append(self.radio_list)
         fields.append(Label(text='Message:'))
         fields.append(self.text_area)
+        return fields
+
+    def open_dialog(self, key, title, include_direction=False):
+        self.current_dialog_key = key
+        full_title = f"{self.active_layer_name}: {title}"
+        existing_messages = bool(self.data_dict[self.active_layer_name][key])
+        fields = self.build_fields(include_direction=include_direction)
+        # self.current_messages_display.text = yaml.dump(self.data_dict[self.active_layer_name][key], default_flow_style=False, sort_keys=True)
+        # fields.append(self.current_messages_display)
+        buttons = [
+            Button(text='Add', handler=functools.partial(self.add, key)),
+            Button(text='Cancel', handler=self.cancel),
+            Button(text="Clear", handler=functools.partial(self.clear_type, key)),
+        ]
+        if existing_messages:
+            full_title += " -- Ctrl-S to edit, Ctrl-D to delete"
+            buttons.append(Button(text='Edit', handler=functools.partial(self.open_message_selector, "edit")))
+            buttons.append(Button(text='Delete', handler=functools.partial(self.open_message_selector, "delete")))
+        self.dialog = Dialog(
+            title=full_title,
+            body=HSplit(fields),
+            buttons=buttons
+        )
+        self.app.layout = Layout(self.dialog, focused_element=self.dialog)
+
+    def edit_dialog(self, key, index, title, include_direction=False):
+        self.reset_values()
+        message = self.data_dict[self.active_layer_name][key][index]
+        self.text_area.text = message['message']
+        if 'direction' in message:
+            self.radio_list.current_value = message['direction']
+        fields = self.build_fields(include_direction)
+        buttons = [
+            Button(text='OK', handler=functools.partial(self.edit, key, index)),
+            Button(text='Cancel', handler=self.cancel),
+        ]
         self.dialog = Dialog(
             title=f"{self.active_layer_name}: {title}",
             body=HSplit(fields),
-            buttons=[
-                Button(text='Add', handler=functools.partial(self.add, key)),
-                Button(text='Cancel', handler=self.cancel),
-                Button(text=f"Clear {key} messages", handler=functools.partial(self.clear_type, key), width=25),
-            ]
+            buttons=buttons
         )
         self.app.layout = Layout(self.dialog, focused_element=self.dialog)
 
@@ -145,14 +186,53 @@ class DebugAceTui:
         self.dialog = None
 
     def add(self, key):
+        new_message = self.get_message_from_dialog(key)
+        self.data_dict[self.active_layer_name][key].append(new_message)
+        self.update_after_change(key)
+
+    def edit(self, key, index):
+        new_message = self.get_message_from_dialog(key)
+        self.data_dict[self.active_layer_name][key][index] = new_message
+        self.update_after_change(key)
+
+    def get_message_from_dialog(self, key):
         new_message = {'message': self.text_area.text}
         if key in ['request', 'response']:
             new_message['direction'] = self.radio_list.current_value
         self.reset_values()
-        self.data_dict[self.active_layer_name][key].append(new_message)
+        return new_message
+
+    def update_after_change(self, key):
+        self.current_dialog_key = None
         self.update_output_display()
         self.app.layout = self.layout
         self.dialog = None
+
+    def open_message_selector(self, action):
+        key = self.current_dialog_key
+        values = [(i, yaml.dump(msg, default_flow_style=False, sort_keys=True)) for i, msg in enumerate(self.data_dict[self.active_layer_name][key])]
+        radio_list = RadioList(values)
+
+        def handler():
+            index = radio_list.current_value
+            if action == "edit":
+                self.edit_dialog(key, index, f"Edit {key.upper()} message", include_direction='direction' in self.data_dict[self.active_layer_name][key][index])
+            elif action == "delete":
+                self.delete_message(key, index)
+
+        dialog = Dialog(
+            title=f"Select a message to {action}",
+            body=HSplit([Label(text='Message:'), radio_list]),
+            buttons=[
+                Button(text='OK', handler=handler),
+                Button(text='Cancel', handler=self.cancel),
+            ]
+        )
+        self.app.layout = Layout(dialog, focused_element=dialog)
+
+    def delete_message(self, key, index):
+        del self.data_dict[self.active_layer_name][key][index]
+        self.update_after_change(key)
 
     def update_output_display(self):
         text = []
@@ -187,6 +267,7 @@ class DebugAceTui:
         self.layout = Layout(root_container)
         self.app.layout = self.layout
         self.app.run()
+
 
 if __name__ == "__main__":
     DebugAceTui().run()
