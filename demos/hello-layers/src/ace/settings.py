04f6616c9893749462a312a525d84570025d2a10
Chad Phillips <chad@apartmentlines.com>
2023-10-24 11:35:08 -0400

    Expose RabbitMQ creds as env vars

--- demos/hello-layers/src/ace/settings.py
+++ demos/hello-layers/src/ace/settings.py
@@ -1,13 +1,16 @@
+import os
 from typing import List
 from pydantic_settings import BaseSettings
+
+from ace import constants
 
 
 class Settings(BaseSettings):
     name: str
     label: str
-    amqp_host_name: str = "rabbitmq"
-    amqp_username: str = "rabbit"
-    amqp_password: str = "carrot"
+    amqp_host_name: str = os.getenv('ACE_RABBITMQ_HOSTNAME') or constants.DEFAULT_RABBITMQ_HOSTNAME
+    amqp_username: str = os.getenv('ACE_RABBITMQ_USERNAME') or constants.DEFAULT_RABBITMQ_USERNAME
+    amqp_password: str = os.getenv('ACE_RABBITMQ_PASSWORD') or constants.DEFAULT_RABBITMQ_PASSWORD
     logging_queue: str = "logging-queue"
     resource_log_queue: str = "resource-log-queue"
     log_dir: str = "/var/log/ace"
