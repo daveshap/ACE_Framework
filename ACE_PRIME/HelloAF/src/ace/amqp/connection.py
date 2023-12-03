80dc90a5ae8aebbec90ba5e1a07d15f3f0cd4770
Chad Phillips <chad@apartmentlines.com>
2023-12-03 11:06:04 -0500

    refactor connection into class

--- ACE_PRIME/HelloAF/src/ace/amqp/connection.py
+++ ACE_PRIME/HelloAF/src/ace/amqp/connection.py
@@ -4,35 +4,42 @@ import aio_pika
 from ace.settings import Settings
 from ace.logger import Logger
 
-logger = Logger(__name__)
+DEFAULT_HEARTBEAT = 600
+DEFAULT_BLOCKED_CONNECTION_TIMEOUT = 300
 
 
-async def get_connection(settings: Settings,
-                         loop=asyncio.get_event_loop(),
-                         max_retries=5,
-                         delay_factor=2,
-                         heartbeat=600,
-                         blocked_connection_timeout=300,
-                         ):
+class AMQPConnectionManager:
 
-    host = settings.amqp_host_name
-    username = settings.amqp_username
-    password = settings.amqp_password
-    connection = None
-    retries = 0
+    def __init__(self,
+                 settings: Settings,
+                 ):
+        self.log = Logger(self.__class__.__name__)
+        self.settings = settings
 
-    while retries < max_retries:
-        try:
-            connection = await aio_pika.connect_robust(
-                f"amqp://{username}:{password}@{host}",
-                heartbeat=heartbeat,
-                blocked_connection_timeout=blocked_connection_timeout,
-            )
-            logger.info(f"{settings.name} connection established...")
-            return connection
-        except (aio_pika.exceptions.AMQPConnectionError, aio_pika.exceptions.AMQPChannelError) as e:
-            print(f"Connection attempt {retries + 1} failed with error: {e}")
-            retries += 1
-            await asyncio.sleep(retries * delay_factor)  # Exponential backoff
+    async def get_connection(self,
+                             max_retries=5,
+                             delay_factor=2,
+                             heartbeat=DEFAULT_HEARTBEAT,
+                             blocked_connection_timeout=DEFAULT_BLOCKED_CONNECTION_TIMEOUT,
+                             **kwargs,
+                             ):
+        connection = None
+        retries = 0
+        while retries < max_retries:
+            try:
+                connection = await aio_pika.connect_robust(
+                    host=self.settings.amqp_host_name,
+                    login=self.settings.amqp_username,
+                    password=self.settings.amqp_password,
+                    heartbeat=heartbeat,
+                    blocked_connection_timeout=blocked_connection_timeout,
+                    **kwargs,
+                )
+                self.log.info(f"{self.settings.name} connection established...")
+                return connection
+            except (aio_pika.exceptions.AMQPConnectionError, aio_pika.exceptions.AMQPChannelError) as e:
+                self.log.error(f"Connection attempt {retries + 1} failed with error: {e}")
+                retries += 1
+                await asyncio.sleep(retries * delay_factor)  # Exponential backoff
 
-    raise Exception(f"Failed to establish a connection and channel after maximum retries: {max_retries}.")
+        raise Exception(f"Failed to establish a connection after maximum retries: {max_retries}.")
