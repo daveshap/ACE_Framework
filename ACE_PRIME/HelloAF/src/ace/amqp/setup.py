958573fa964192b36ae4aa9b3dfe70ade7d303e1
Chad Phillips <chad@apartmentlines.com>
2023-12-05 00:09:12 -0600

    refactor setup to be more robust, fill out bus config

--- ACE_PRIME/HelloAF/src/ace/amqp/setup.py
+++ ACE_PRIME/HelloAF/src/ace/amqp/setup.py
@@ -5,12 +5,6 @@ import aio_pika
 
 from ace.logger import Logger
 
-EXCHANGE_TYPE_MAP = {
-    'fanout': aio_pika.ExchangeType.FANOUT,
-    'topic': aio_pika.ExchangeType.TOPIC,
-    'direct': aio_pika.ExchangeType.DIRECT
-}
-
 
 class AMQPSetupManager:
 
@@ -36,57 +30,27 @@ class AMQPSetupManager:
         for name, c in self.exchanges_config.items():
             config = copy.deepcopy(c)
             exchange_type = config.pop('type', 'fanout')
-            await self.setup_exchange(channel, name, exchange_type, **config)
+            durable = config.pop('durable', True)
+            await self.setup_exchange(channel, name, exchange_type, durable, **config)
 
-    async def setup_exchange(self, channel: aio_pika.Channel, name: str, exchange_type: str = 'fanout', **kwargs):
+    async def setup_exchange(self,
+                             channel: aio_pika.Channel,
+                             name: str,
+                             exchange_type: str = 'fanout',
+                             durable: bool = True,
+                             **kwargs,
+                             ):
         exchange_name = self.make_exchange_name(name)
-        self.log.debug(f"Set up exchange: {exchange_name}, type: {exchange_type}, kwargs: {kwargs}")
-        self.exchanges[name] = await channel.declare_exchange(exchange_name, EXCHANGE_TYPE_MAP[exchange_type], **kwargs)
-        if exchange_type == 'fanout':
-            await self.bind_monitoring_queues(channel, exchange_name)
+        self.log.debug(f"Set up: {exchange_name}, type: {exchange_type}, durable: {durable}, kwargs: {kwargs}")
+        self.exchanges[name] = await channel.declare_exchange(exchange_name, exchange_type, durable=durable, **kwargs)
 
     async def teardown_exchanges(self, channel: aio_pika.Channel):
-        for exchange in self.exchanges.values():
-            await self.teardown_exchange(channel, exchange)
-        await self.teardown_monitoring_queues(channel)
+        for name, exchange in self.exchanges.items():
+            await self.teardown_exchange(channel, name, exchange)
 
-    async def teardown_exchange(self, channel: aio_pika.Channel, exchange: aio_pika.Exchange):
-        # NOTE: exchange._type is not public, but convenient for this task
-        if exchange._type == 'fanout':
-            await self.unbind_monitoring_queues(channel, exchange.name)
+    async def teardown_exchange(self, channel: aio_pika.Channel, name: str, exchange: aio_pika.Exchange):
         await exchange.delete()
-        self.log.debug(f"Tore down exchange: {exchange.name}")
-
-    async def teardown_monitoring_queues(self, channel: aio_pika.Channel):
-        if self.settings.system_integrity_queue:
-            system_integrity_queue = await channel.declare_queue(self.settings.system_integrity_queue, durable=True)
-            await system_integrity_queue.delete(if_empty=False, if_unused=False)
-            self.log.debug(f"Removed queue {self.settings.system_integrity_queue}")
-        if self.settings.logging_queue:
-            logging_queue = await channel.declare_queue(self.settings.logging_queue, durable=True)
-            await logging_queue.delete(if_empty=False, if_unused=False)
-            self.log.debug(f"Removed queue {self.settings.logging_queue}")
-
-    async def bind_monitoring_queues(self, channel: aio_pika.Channel, exchange_name):
-        if self.settings.system_integrity_queue:
-            system_integrity_queue = await channel.declare_queue(self.settings.system_integrity_queue, durable=True)
-            await system_integrity_queue.bind(exchange_name)
-            self.log.debug(f"Bound {self.settings.system_integrity_queue} to exchange {exchange_name}")
-
-        if self.settings.logging_queue:
-            logging_queue = await channel.declare_queue(self.settings.logging_queue, durable=True)
-            await logging_queue.bind(exchange_name)
-            self.log.debug(f"Bound {self.settings.logging_queue} to exchange {exchange_name}")
-
-    async def unbind_monitoring_queues(self, channel: aio_pika.Channel, exchange_name: str):
-        if self.settings.system_integrity_queue:
-            system_integrity_queue = await channel.declare_queue(self.settings.system_integrity_queue, durable=True)
-            await system_integrity_queue.unbind(exchange_name)
-            self.log.debug(f"Unbound {self.settings.system_integrity_queue}")
-        if self.settings.logging_queue:
-            logging_queue = await channel.declare_queue(self.settings.logging_queue, durable=True)
-            await logging_queue.unbind(exchange_name)
-            self.log.debug(f"Unbound {self.settings.logging_queue}")
+        self.log.debug(f"Tore down: {exchange.name}")
 
     async def setup_queues(self, channel: aio_pika.Channel):
         for name, c in self.queues_config.items():
@@ -114,27 +78,24 @@ class AMQPSetupManager:
         self.log.debug(f"Removed queue {queue.name}, durable: {queue.durable}")
 
     async def setup_queue_bindings(self, channel: aio_pika.Channel):
-        for binding in self.queue_bindings_config:
-            config = copy.deepcopy(binding)
-            queue_name = config.pop('queue')
-            exchange_name = config.pop('exchange')
-            await self.setup_queue_binding(channel, queue_name, exchange_name, **config)
+        for exchange, bindings in self.queue_bindings_config.items():
+            for queue_name, kwargs in bindings.get('queues', {}).items():
+                await self.setup_queue_binding(channel, queue_name, exchange, **kwargs)
 
-    async def setup_queue_binding(self, channel: aio_pika.Channel, queue_name: str, exchange_name: str, **kwargs):
-        await self.queues[queue_name].bind(self.make_exchange_name(exchange_name), **kwargs)
-        self.queue_bindings.append({
-            'queue': queue_name,
-            'exchange': exchange_name,
-        })
-        self.log.debug(f"Bound queue {queue_name} to exchange {exchange_name}, kwargs: {kwargs}")
+    async def setup_queue_binding(self, channel: aio_pika.Channel, queue_name: str, exchange: str, **kwargs):
+        exchange_name = self.make_exchange_name(exchange)
+        await self.queues[queue_name].bind(exchange_name, **kwargs)
+        self.log.debug(f"Bound queue {queue_name} to {exchange_name}, kwargs: {kwargs}")
 
     async def teardown_queue_bindings(self, channel: aio_pika.Channel):
-        for binding in self.queue_bindings:
-            await self.teardown_queue_binding(channel, binding['queue'], binding['exchange'])
+        for exchange, bindings in self.queue_bindings_config.items():
+            for queue_name in bindings.get('queues', {}).keys():
+                await self.teardown_queue_binding(channel, queue_name, exchange)
 
-    async def teardown_queue_binding(self, channel: aio_pika.Channel, queue_name: str, exchange_name: str):
-        await self.queues[queue_name].unbind(self.make_exchange_name(exchange_name))
-        self.log.debug(f"Unbound queue {queue_name} from exchange {exchange_name}")
+    async def teardown_queue_binding(self, channel: aio_pika.Channel, queue_name: str, exchange: str):
+        exchange_name = self.make_exchange_name(exchange)
+        await self.queues[queue_name].unbind(exchange_name)
+        self.log.debug(f"Unbound queue {queue_name} from {exchange_name}")
 
     async def setup_resource_pathways(self, channel: aio_pika.Channel):
         for name, c in self.resources_config.items():
@@ -144,7 +105,7 @@ class AMQPSetupManager:
 
     async def setup_resource_pathway(self, channel: aio_pika.Channel, resource_name: str, pathway: str, exchanges: list):
         source_exchange_name = self.make_resource_pathway_name(resource_name, pathway)
-        pathway = await channel.declare_exchange(source_exchange_name, aio_pika.ExchangeType.FANOUT)
+        pathway = await channel.declare_exchange(source_exchange_name, aio_pika.ExchangeType.FANOUT, durable=True)
         self.resource_pathways.setdefault(resource_name, {})
         self.resource_pathways[resource_name][pathway] = {
             'pathway': pathway,
@@ -153,7 +114,7 @@ class AMQPSetupManager:
         for name in exchanges:
             exchange_name = self.make_exchange_name(name)
             await self.exchanges[name].bind(source_exchange_name)
-            self.log.debug(f"Bound queue {exchange_name} to exchange {source_exchange_name}")
+            self.log.debug(f"Bound queue {exchange_name} to {source_exchange_name}")
 
     async def teardown_resource_pathways(self, channel: aio_pika.Channel):
         for name, pathways in self.resource_pathways.items():
@@ -164,6 +125,6 @@ class AMQPSetupManager:
         pathway = pathway_data['pathway']
         for exchange_name in pathway_data['exchanges']:
             await self.exchanges[exchange_name].unbind(pathway.name)
-            self.log.debug(f"Unbound exchange {pathway.name} from exchange {self.exchanges[exchange_name].name}")
+            self.log.debug(f"Unbound {pathway.name} from {self.exchanges[exchange_name].name}")
         await pathway.delete()
-        self.log.debug(f"Removed exchange {pathway.name}")
+        self.log.debug(f"Removed {pathway.name}")
